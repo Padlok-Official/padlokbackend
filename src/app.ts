66c9bd7154ddd8ee3f872c50c71d5512d8a2d478
Bernard Baah <benbaah104@gmail.com>
2026-03-11 01:52:01 +0000

    update

--- src/app.ts
+++ src/app.ts
@@ -34,8 +34,8 @@ app.use(
   }),
 );
 app.use(compression() as unknown as express.RequestHandler);
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 app.use(generalLimiter);
