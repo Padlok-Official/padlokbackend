59149d5dc937c822b16719a318437a7ac6999726
Bernard Baah <benbaah104@gmail.com>
2026-03-13 16:52:27 +0000

    access key manager

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -16,8 +16,6 @@ export const broadcastNotification = async (
             });
         }
 
-        console.log("update")
-
         const navigationPayload = screen ? { screen, params } : undefined;
         const result = await NotificationService.broadcastNotification(title, body, navigationPayload);
 
